f70857354c6b969659397d7dbd5d47d687bace17
Nikhil Verma <jsnikhil0@gmail.com>
2025-05-27 10:39:48 +0530

    cors

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(
       "http://127.0.0.1:3001",
       "http://127.0.0.1:3000",
       "https://ddc9833g-5173.inc1.devtunnels.ms",
+      "https://multivendor-eta.vercel.app",
+      "https://mltivender-production.up.railway.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
